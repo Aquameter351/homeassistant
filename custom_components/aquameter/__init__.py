15e7fe02d48a665cd5eba5240abbcf03413ca3dc
Aquameter351 <geral@aquameter.pt>
2026-06-23 23:26:47 +0100

    update init sensor

--- custom_components/aquameter/__init__.py
+++ custom_components/aquameter/__init__.py
@@ -1,29 +1,49 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.components import bluetooth
 
-from .const import DOMAIN
+from .bluetooth import AquaMeterBluetoothClient
+from .const import DOMAIN, SERVICE_UUID
+
+PLATFORMS = ["sensor"]
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-) -> bool:
-    await hass.config_entries.async_forward_entry_setups(
-        entry,
-        ["sensor"],
-    )
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.data.setdefault(DOMAIN, {})
+
+    ble_device = None
+
+    for info in bluetooth.async_discovered_service_info(hass, connectable=True):
+        uuids = [u.lower() for u in info.service_uuids]
+        if SERVICE_UUID.lower() in uuids:
+            ble_device = info.device
+            break
+
+    if ble_device is None:
+        raise ConfigEntryNotReady("AquaMeter Bluetooth device not found")
+
+    client = AquaMeterBluetoothClient(ble_device)
+    await client.connect()
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        "client": client,
+    }
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
-async def async_unload_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-) -> bool:
-    return await hass.config_entries.async_unload_platforms(
-        entry,
-        ["sensor"],
-    )
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    data = hass.data[DOMAIN].pop(entry.entry_id, None)
+
+    if data:
+        await data["client"].disconnect()
+
+    return unload_ok
--- custom_components/aquameter/sensor.py
+++ custom_components/aquameter/sensor.py
@@ -1,17 +1,53 @@
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import UnitOfVolume, PERCENTAGE
+
+from .const import DOMAIN
+
+
+SENSORS = [
+    ("water", "Water Remaining", UnitOfVolume.LITERS),
+    ("percent", "Tank Level", PERCENTAGE),
+    ("flow", "Flow Rate", "L/min"),
+    ("today", "Today Consumption", UnitOfVolume.LITERS),
+]
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    async_add_entities([
-        AquaMeterSensor("Water Remaining", "L"),
-        AquaMeterSensor("Tank Level", "%"),
-        AquaMeterSensor("Flow Rate", "L/min"),
-        AquaMeterSensor("Today Consumption", "L"),
-    ])
+    client = hass.data[DOMAIN][entry.entry_id]["client"]
+
+    entities = [
+        AquaMeterSensor(client, key, name, unit)
+        for key, name, unit in SENSORS
+    ]
+
+    async_add_entities(entities)
 
 
 class AquaMeterSensor(SensorEntity):
-    def __init__(self, name, unit):
+    def __init__(self, client, key, name, unit):
+        self.client = client
+        self.key = key
+
         self._attr_name = f"AquaMeter {name}"
         self._attr_native_unit_of_measurement = unit
         self._attr_native_value = None
+
+        client.register_callback(self._handle_update)
+
+    def _handle_update(self, data):
+        if self.key == "water":
+            self._attr_native_value = data.water
+
+        elif self.key == "percent":
+            if data.capacity > 0:
+                self._attr_native_value = round((data.water / data.capacity) * 100, 1)
+            else:
+                self._attr_native_value = None
+
+        elif self.key == "flow":
+            self._attr_native_value = data.flow
+
+        elif self.key == "today":
+            self._attr_native_value = data.today
+
+        self.schedule_update_ha_state()
