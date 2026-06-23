dea3a27387bffd53cd70cd94b5849bd5dfced510
Aquameter351 <geral@aquameter.pt>
2026-06-23 22:55:26 +0100

    Add AquaMeter sensor entities

--- custom_components/aquameter/sensor.py
+++ custom_components/aquameter/sensor.py
@@ -1,4 +1,17 @@
-Water Remaining
-Tank Level
-Flow Rate
-Today Consumption
+from homeassistant.components.sensor import SensorEntity
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    async_add_entities([
+        AquaMeterSensor("Water Remaining", "L"),
+        AquaMeterSensor("Tank Level", "%"),
+        AquaMeterSensor("Flow Rate", "L/min"),
+        AquaMeterSensor("Today Consumption", "L"),
+    ])
+
+
+class AquaMeterSensor(SensorEntity):
+    def __init__(self, name, unit):
+        self._attr_name = f"AquaMeter {name}"
+        self._attr_native_unit_of_measurement = unit
+        self._attr_native_value = None
